8ae1bda62416fa719e3575e0444ee6b0c8efbf29
LuckPumpkin <zhaoyn0201@163.com>
2021-03-09 17:26:25 +0800

    基础搭建

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: yanan.zhao
+ * @Date: 2021-03-09 16:25:48
+ * @LastEditors: yanan.zhao
+ * @LastEditTime: 2021-03-09 17:07:08
+ */
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello,nangua!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
