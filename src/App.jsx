897ae28d9c77c04981fe1110814a4e0e833fa312
LuckPumpkin <zhaoyn0201@163.com>
2021-03-09 19:35:26 +0800

    dva-router

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@
  * @Author: yanan.zhao
  * @Date: 2021-03-09 16:25:48
  * @LastEditors: yanan.zhao
- * @LastEditTime: 2021-03-09 17:07:08
+ * @LastEditTime: 2021-03-09 19:20:53
  */
 import React, { Component } from "react";
-
+ 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello,nangua!</h1>
-      </div>
+      <h1>hello,nangua!</h1>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@
  * @Author: yanan.zhao
  * @Date: 2021-03-09 14:15:04
  * @LastEditors: yanan.zhao
- * @LastEditTime: 2021-03-09 17:22:53
+ * @LastEditTime: 2021-03-09 19:21:45
  */
-import React from 'react'
-import ReactDom from 'react-dom'
-import {Router, Route, Link } from "react-router"
+import dva from 'dva'
 import { createBrowserHistory } from 'history'
-import App from './App.jsx'
-import product from './routes/Products.js'
 
-ReactDom.render(
-  <Router history={ createBrowserHistory() }>
-    <Route path='/' component={App}/>
-    <Route path="/products" component={product} />
-  </Router>,
-  document.getElementById('root')
-)
+const app = dva({
+  history: createBrowserHistory(),
+})
+
+
+
+
+app.router(require(`./route.js`).default)
+
+
+app.start('#root')
